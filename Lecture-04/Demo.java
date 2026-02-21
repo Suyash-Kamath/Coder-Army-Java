d3c49bd9b94c551327cfc98d85b28d785a23b9d4
Suyash Kamath <suyashkamath9@gmail.com>
2026-02-21 19:02:00 +0530

    Lecture 4

---
+++ Lecture-04/Demo.java
@@ -0,0 +1,10 @@
+
+
+public class Demo {
+    public static void main(String[] args) {
+        float f = 8.125f;
+        System.out.printf("%.20f%n",f);
+        float g = 0.7f;
+        System.out.printf("%.20f%n",g);
+    }
+}

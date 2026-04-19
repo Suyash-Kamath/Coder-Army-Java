a398ff58aa3858a54c66776feb48843d4977e71b
Suyash Kamath <suyashkamath9@gmail.com>
2026-04-19 23:17:06 +0530

    Null Pointer Exception in Auto Boxing

---
+++ lecture_18_autoboxing_abstract_classes_and_POJOs/code/NullPointerExceptionInAutoBoxing.java
@@ -0,0 +1,14 @@
+package code;
+
+public class NullPointerExceptionInAutoBoxing {
+    public static void main(String[] args) {
+        // Integer x = 10;
+        Integer x = null; // object null store kar sakte hai , null ka meaning no value
+        int y = x; // this cannot store null value 
+        // null pe koi bhi method call karne jaaoge toh runtime pe null pointer exception aayega 
+
+        System.out.println(y);
+
+    }
+    
+}
